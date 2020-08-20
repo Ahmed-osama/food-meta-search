f54c9db1c83722e134793c8fd1b5aa1029872167
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-21 03:46:42 +0400

    use next api

--- pages/search-results.js
+++ pages/search-results.js
@@ -2,9 +2,10 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useState, useEffect } from "react";
 import { useApi } from "@/hooks/api.hook";
+import MealListItem from "@/components/MealListItem/MealListItem";
 export default function SearchResults() {
   const mealsList = useApi({
-    url: "https://5f3ed66c13a9640016a69219.mockapi.io/api/meals",
+    url: "/api/meals",
     defaultData: [],
   });
   useEffect(mealsList.call, []);
@@ -18,15 +19,8 @@ export default function SearchResults() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ul>
-        {isLoading ? <h2>Looading..</h2> : null}
-        {hasData
-          ? data.map(({ meal_name }) => (
-              <li>
-                <img src={"https://source.unsplash.com/100x100/?meal"} alt="" />
-                {meal_name}
-              </li>
-            ))
-          : null}
+        {isLoading ? <h2>Loading..</h2> : null}
+        {hasData ? data.map((props) => <MealListItem {...props} />) : null}
       </ul>
     </>
   );
