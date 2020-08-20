ff69cc749e797b2bf6b2213b9d457bf02cf56b45
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-21 02:36:52 +0400

    redirection page

---
+++ pages/redirection.js
@@ -0,0 +1,11 @@
+export default function Redirection() {
+  return (
+    <>
+      <Head>
+        <title>seera foods</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Redirection</h1>
+    </>
+  );
+}
