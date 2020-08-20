ac8df1441ef32ce395d9672091b4285145a47506
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-21 02:32:55 +0400

    add api hook

---
+++ hooks/api.hook.js
@@ -0,0 +1,50 @@
+import { useEffect, useState, useCallback } from "react";
+import { useStore } from "./store.hook";
+import _isEmpty from "lodash/isEmpty";
+const API_STATE = {
+  INITIAL: "INITIAL",
+  LOADING: "LOADING",
+  SUCCESS: "SUCCESS",
+  FAILED: "FAILED",
+};
+export function useApi({ url, defaultData }) {
+  const [error, setError] = useState(null);
+  //Data Observable
+  const dataStore = useStore({
+    initial: defaultData,
+    computed: {
+      hasData: (data) => !_isEmpty(data) && data !== null,
+    },
+  });
+  // Api Observable
+  const apiStore = useStore({
+    initial: API_STATE.INITIAL,
+    computed: {
+      isLoading: (state) => state === API_STATE.LOADING,
+      isFetched: (state) => state === API_STATE.SUCCESS,
+      isFailed: (state) => state === API_STATE.FAILED,
+    },
+  });
+
+  // Call api Action
+  const call = useCallback(() => {
+    apiStore.setState(API_STATE.LOADING);
+    fetch(url)
+      .then((data) => data.json())
+      .then((data) => {
+        dataStore.setState(data);
+        apiStore.setState(API_STATE.SUCCESS);
+      })
+      .catch((err) => {
+        setError(err);
+        apiStore.etState(API_STATE.FAILED);
+      });
+  });
+
+  return {
+    data: dataStore.state,
+    ...apiStore.computed,
+    ...dataStore.computed,
+    call,
+  };
+}
