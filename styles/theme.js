f654816ffdfa0d926cefd6bb88fb3d2b37a7acf6
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-21 19:57:09 +0400

    css theeme and remove files

---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#BC312C",
+    },
+  },
+});
+
+export default theme;
