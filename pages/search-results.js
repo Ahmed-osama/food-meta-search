8bb4a95d0288705c8b831b6da920e65d058fd83b
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-21 19:56:51 +0400

    react material in search resuts page

--- pages/search-results.js
+++ pages/search-results.js
@@ -1,27 +1,43 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useApi } from "@/hooks/api.hook";
 import MealListItem from "@/components/MealListItem/MealListItem";
+import { Grid, CircularProgress } from "@material-ui/core";
+import Template from "@/components/Template/Template";
+import Header from "@/components/Header/Header";
 export default function SearchResults() {
   const mealsList = useApi({
     url: "/api/meals",
     defaultData: [],
   });
   useEffect(mealsList.call, []);
-
   const { data, isFetched, isLoading, hasData } = mealsList;
   return (
     <>
-      <h1>SearchResults</h1>
-      <Head>
-        <title>seera foods</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <ul>
-        {isLoading ? <h2>Loading..</h2> : null}
-        {hasData ? data.map((props) => <MealListItem {...props} />) : null}
-      </ul>
+      <Template header={Header}>
+        <Grid
+          direction="column"
+          justify="flex-start"
+          alignItems="stretch"
+          spacing={2}
+          xs
+        >
+          {isLoading ? (
+            <Grid
+              direction="row"
+              justify="center"
+              alignItems="center"
+              xs
+              style={{ alignSelf: "stretch", height: "calc(100vh - 56px)" }}
+              container
+            >
+              <CircularProgress disableShrink />
+            </Grid>
+          ) : null}
+          {hasData
+            ? data.map((props) => <MealListItem {...props} key={props.id} />)
+            : null}
+        </Grid>
+      </Template>
     </>
   );
 }
