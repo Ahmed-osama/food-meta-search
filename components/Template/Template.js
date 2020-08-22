c837959b9efc6c9e4133a8531a37c5ed625888c0
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-23 03:38:33 +0400

    padding prop in template component

--- components/Template/Template.js
+++ components/Template/Template.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Grid } from "@material-ui/core";
-const Template = ({ header, children }) => {
+const Template = ({ header, children, paddingTop = 56 }) => {
   return (
-    <Grid spacing="0" direction="column" style={{ paddingTop: "56px" }}>
+    <Grid
+      spacing="0"
+      direction="column"
+      style={{ paddingTop: `${paddingTop}px` }}
+    >
       <Grid item>{header()}</Grid>
       <Grid item container xs>
         {children}
