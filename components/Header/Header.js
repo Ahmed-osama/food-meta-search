d0ec2aca386f6433826a862e130c360e73396b6c
Ahmed-osama <ahmod.osama@gmail.com>
2020-08-23 03:37:59 +0400

    header text prop

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,11 +2,16 @@ import React from "react";
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import MoreIcon from "@material-ui/icons/More";
-const Header = () => {
+import { useRouter } from "next/router";
+
+const Header = ({ headerText }) => {
+  const router = useRouter();
   return (
     <AppBar position="fixed" color="primary">
       <Toolbar>
-        <Typography variant="h6">Seera Foods</Typography>
+        <Typography variant="h6" onClick={() => router.push("/")}>
+          {headerText}
+        </Typography>
         <div style={{ flexGrow: 1 }} />
         <IconButton edge="end" color="inherit">
           <MenuIcon />
